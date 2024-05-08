ddd66d47b737b2db530859230f2a3bfffe17c957
bechong <bechong@paloaltonetworks.com>
2024-05-08 22:01:41 +0800

    initial commit

---
+++ variable.tf
@@ -0,0 +1,27 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+variable "project_id" {
+    description = "project id"
+}
+
+variable "region" {
+  description = "region"
+}
+
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
+}
